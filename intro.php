07680332a583b8bb56200dae97f6f87f1fe3d12c
Charles Cordova <131734392+CharlesCordova22@users.noreply.github.com>
2023-05-01 23:14:49 -0700

    Add files via upload

---
+++ intro.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/8c89857bb1.js" crossorigin="anonymous"></script>
+        <link rel="icon" href="image/c-low-resolution-logo-white-on-transparent-background.png" type="image/icon type">
+        <title>Herpes</title>
+    </head>
+<body>
+    <!-- -----------------------INTRO----------------------- -->
+    <div id="header">
+        <div class="container">
+            <nav>
+                <img src="image/herpes-low-resolution-logo-white-on-transparent-background.png" class="logo">
+                <ul id="sidemenu">
+                    <li><a href="#header">Home</a></li>
+                    <li><a href="#about_me">About</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+            <div class="text-introduce">
+                <h1 >Be Bold. Be Confident. Be You.<br>Discover Your Style with Us.</h1>
+            </div>
+            <div class="get_started" >
+                    <input type="submit" value="Get Started!">
+                </div>
+            <div class="home-pic">
+                <img src="image/imgbin_smart-casual-male-shirt-clothing-png.png" >
+            </div>
+        </div>
+    </div><br>
+</body>
+</html>
